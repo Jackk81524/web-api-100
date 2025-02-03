c8d6f0a940d78d34f883cb157bb3e3bbce7b99ee
Jack Knox <jackknox81524@gmail.com>
2025-02-03 16:31:47 -0500

    End of Day1

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Status/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCatalog.Api.Status;
+
+// Must be public and must extend* ControllerBase
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        var response = new StatusResponse(DateTimeOffset.Now, "Looks Good!");
+        return Ok(response);
+    }
+}
+
+public record StatusResponse(DateTimeOffset LastChecked, String Message);
+
---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Status/CanGetTheStatus.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Alba;
+
+namespace SoftwareCatalog.Tests.Status;
+public class CanGetTheStatus
+{
+    [Fact]
+    public async Task GetsA200WhenGettingTheStatus()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+
+}
