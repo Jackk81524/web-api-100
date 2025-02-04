9ca1216bee3d119a5dc5e4ac10ad8a63e2d2b795
Jack Knox <jackknox81524@gmail.com>
2025-02-04 15:45:48 -0500

    setup docker container

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCatalog.Api.Catalog;
+
+public class CatalogController : ControllerBase
+{
+    [HttpPost("/vendors/microsoft/opensource")]
+    public async Task<ActionResult> AddItemToCatalogAsync(
+        [FromBody] CatalogItemRequestModel request)
+    {
+        var fakeResponse = new CatalogItemResponseDetailsModel
+        {
+            Id = Guid.NewGuid(),
+            License = CatalogItemLicenseTypes.OpenSource,
+            Name = request.Name,
+            Vendor = "Microsoft"
+        };
+        return StatusCode(201, fakeResponse);
+    }
+}
+
+
+public record CatalogItemRequestModel
+{
+    public string Name { get; set; } = string.Empty;
+}
+
+public enum CatalogItemLicenseTypes { OpenSource, Free, Paid };
+
+public record CatalogItemResponseDetailsModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Vendor { get; set; } = string.Empty;
+
+    public CatalogItemLicenseTypes License { get; set; }
+}
